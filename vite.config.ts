f7baabe8edfc07fc0523d6d0cd29b664d37fbc51
Crisxz1891 <crisxz1891@gmail.com>
2025-07-24 18:25:24 -0300

    Configurar despliegue a GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -8,4 +8,5 @@ export default defineConfig({
     react(),
     tailwindcss()
   ],
+  base: 'https://github.com/Cristian1891/valores_sinai.git' // reemplaza TU_REPO por el nombre exacto del repo GitHub
 })
